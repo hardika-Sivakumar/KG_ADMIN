87c85b0d2b5ee814d4ad41875ac7043e5e07cf98
Ajay Kumar <ajay.kumar@solitontech.local>
2025-07-14 18:26:43 +0530

    updated the form fields

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 
 
-const CONFIG={
-    PORT:process.env.PORT || 3000,
+const CONFIG = {
+    PORT: process.env.PORT || 3000,
+    BACKEND_URL: process.env.BACKEND_URL
 }
 console.log(CONFIG);
 
--- index.js
+++ index.js
@@ -2,27 +2,28 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const session = require('express-session');
-const {CONFIG} = require("./config");
+const { CONFIG } = require("./config");
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, ''));
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '')));
 
 
-app.get("/",async(req,res)=>{
+app.get("/", async (req, res) => {
     return res.render('index');
 })
-app.get("/add-student",async(req,res)=>{
+app.get("/add_student", async (req, res) => {
     return res.render('add-student');
 })
-app.get("/add-teacher",async(req,res)=>{
+app.get("/add_teacher", async (req, res) => {
     return res.render('add-teacher');
 })
 
+app.locals.BACKEND_URL = CONFIG?.BACKEND_URL
 
 app.listen(CONFIG.PORT, () => {
-  console.log(`Server is running at http://localhost:${CONFIG.PORT}`);
+    console.log(`Server is running at http://localhost:${CONFIG.PORT}`);
 
 });
